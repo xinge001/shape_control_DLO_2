cf272df912ecc49a7219c87f98517b10b6c82f69
xinge001 <69582710+xinge001@users.noreply.github.com>
2025-11-19 13:38:53 +0800

    Add files via upload

--- ws_dlo/src/dlo_manipulation_pkg/scripts/GN_model_DLO.py
+++ ws_dlo/src/dlo_manipulation_pkg/scripts/GN_model_DLO.py
@@ -1,23 +1,12 @@
-import copy
-import numpy as np
 import torch
-import tqdm
-from sklearn.model_selection import train_test_split
 
 import torch.nn as nn
-# import cv2
-# import matplotlib.pyplot as plt
-import torch.optim as optim
-import networkx as nx
 import torch_geometric as pyg
 
 import my_functions as mf
 import torch_scatter
 
 from torch.nn.functional import mse_loss
-
-
-
 loss_fn = mse_loss
 
 
@@ -103,7 +92,7 @@ class MySubEdge(pyg.nn.MessagePassing):
         # print("aggregate output", out)
         return (inputs, out)
 
-    def train(self, particle_effect, node_feature, edge_index, edge_feature, y):
+    def train(self, particle_effect, node_feature, edge_index, edge_feature, y, check_conv=True):
         lr = self.lr
 
         edge_all, aggr = self.propagate(edge_index, x=(particle_effect, particle_effect), edge_feature=edge_feature)
@@ -169,20 +158,22 @@ class MySubEdge(pyg.nn.MessagePassing):
             else:
                 return False
 
-        pho = 1.25
-        if_converge = check_convergence(lr)
-
-        # while if_converge:
-        #     print("increase", lr)
-        #     lr = lr * pho
-        #     if_converge = check_convergence(lr)
-
-        while not if_converge:
-            lr = lr / pho
-            # print("decrease", lr)
+        if check_conv:
+            pho = 1.25
             if_converge = check_convergence(lr)
 
-        print("lr", lr)
+            # while if_converge:
+            #     print("increase", lr)
+            #     lr = lr * pho
+            #     if_converge = check_convergence(lr)
+
+            while not if_converge:
+                lr = lr / pho
+                # print("decrease", lr)
+                if_converge = check_convergence(lr)
+
+            print("lr", lr)
+            self.lr = lr
 
         #update law
 
@@ -196,7 +187,6 @@ class MySubEdge(pyg.nn.MessagePassing):
             w21 += self.alpha1*lr*(x2.t() @ e).t()
 
         # print("loss", mse_loss(node_out, y))
-        self.lr = lr
 
         # return node_out
         if self.type=='ee':
@@ -239,7 +229,7 @@ class MySubNode(nn.Module):
 
         return out, phi
 
-    def train(self, x, y):
+    def train(self, x, y, check_conv = True):
         lr = self.lr
 
         xw = self.node_layer(x).detach().clone()
@@ -288,19 +278,21 @@ class MySubNode(nn.Module):
             else:
                 return False
 
-        pho = 1.25
-        if_converge = check_convergence(lr)
-
-        # while if_converge:
-        #     print("increase", lr)
-        #     lr = lr * pho
-        #     if_converge = check_convergence(lr)
-
-        while not if_converge:
-            lr = lr / pho
+        if check_conv:
+            pho = 1.25
             if_converge = check_convergence(lr)
 
-        print("lr", lr)
+            # while if_converge:
+            #     print("increase", lr)
+            #     lr = lr * pho
+            #     if_converge = check_convergence(lr)
+
+            while not if_converge:
+                lr = lr / pho
+                if_converge = check_convergence(lr)
+
+            print("lr", lr)
+            self.lr = lr
 
 
         # update laws
@@ -310,7 +302,252 @@ class MySubNode(nn.Module):
             w1[i, :] = w1[i, :] + self.alpha0*lr*delta_w1.t()
 
         w2 += self.alpha1*lr*(phi.t() @ e).t()
-        self.lr = lr
+
         # print("loss", mse_loss(node_out, y))
 
         return node_out, phi
+
+
+class MySimulator(torch.nn.Module):
+    """Graph Network-based Simulators(GNS)"""
+    def __init__(self,args):
+        super().__init__()
+
+        # self.edge_prop_layer = torch.nn.Linear(350, 100, bias=False)
+        # self.node_prop_layer = torch.nn.Linear(200, 100, bias=False)
+
+        self.args = args
+
+        node_input_dim = args.node_input_dim
+        edge_input_dim = args.edge_input_dim
+
+        subnet0_ee = MySubEdge('ee', edge_input_dim,100,100,2,args)
+        subnet1_ee = MySubEdge('ee', 100,100,100,2,args)
+        subnet2_ee = MySubEdge('ee', 100,100,100,2,args)
+        subnet3_en = MySubNode('en', node_input_dim,100,100,2,args)
+        subnet4_en = MySubNode('en', 100,100,100,2,args)
+
+
+        subnet5_pe = MySubEdge('pe', 300,100,200,2,args)
+        subnet6_pn = MySubNode('pn', 200,100,100,2,args)
+
+        subnet7_pe = MySubEdge('pe', 300,100,200,2,args)
+        subnet8_pn = MySubNode('pn', 200,100,100,2,args)
+
+        subnet9_pe = MySubEdge('pe',  300,100,200,2,args)
+        subnet10_pn = MySubNode('pn', 200,100,100,2,args)
+
+        subnet11_dn = MySubNode('dn', 100, 100, 100, 2,args)
+
+
+        subnet5_pe_v = MySubEdge('pe', 300,100,200,2,args)
+        subnet6_pn_v = MySubNode('pn', 200,100,100,2,args)
+
+        subnet7_pe_v = MySubEdge('pe', 300,100,200,2,args)
+        subnet8_pn_v = MySubNode('pn', 200,100,100,2,args)
+
+        subnet9_pe_v = MySubEdge('pe',  300,100,200,2,args)
+        subnet10_pn_v = MySubNode('pn', 200,100,100,2,args)
+
+        subnet11_dn_v = MySubNode('dn', 100, 100, 100, 2,args)
+
+        # self.layers = torch.nn.ModuleList([MySubEdge('pe', 350,100, 200, 2), MySubNode(200, 100, 2), MySubNode(100, 100, 2)])
+        # self.subnets= torch.nn.ModuleList([subnet0_ee, subnet1_ee, subnet2_ee, subnet3_en, subnet4_en, subnet5_pe, subnet6_pn, subnet5_pe, subnet6_pn, subnet5_pe, subnet6_pn, subnet11_dn])
+        # self.subnets= torch.nn.ModuleList([subnet0_ee, subnet3_en, subnet5_pe, subnet6_pn])
+        self.subnets= torch.nn.ModuleList([subnet0_ee, subnet1_ee, subnet2_ee, subnet3_en, subnet4_en, subnet5_pe, subnet6_pn, subnet7_pe, subnet8_pn,  subnet9_pe, subnet10_pn, subnet11_dn])
+        self.subnets_v= torch.nn.ModuleList([subnet0_ee, subnet1_ee, subnet2_ee, subnet3_en, subnet4_en, subnet5_pe_v, subnet6_pn_v, subnet7_pe_v, subnet8_pn_v,  subnet9_pe_v, subnet10_pn_v, subnet11_dn_v])
+
+
+        # ensure the initial weights are same for main nets, and virtual nets
+        for i, subnet in enumerate(self.subnets_v):
+            if subnet.type == 'pe':
+                subnet.edge_layer.weight.data = self.subnets[i].edge_layer.weight.data.clone()
+                subnet.sudo_node_layer.weight.data = self.subnets[i].sudo_node_layer.weight.data.clone()
+            elif subnet.type == 'pn' or subnet.type == 'dn':
+                subnet.node_layer.weight.data = self.subnets[i].node_layer.weight.data.clone()
+                subnet.sudo_node_layer.weight.data = self.subnets[i].sudo_node_layer.weight.data.clone()
+
+    def my_load_weights(self, checkpoint_path, layers_to_load=[]):
+        # Load the state dictionary from a saved file
+        state_dict = torch.load(checkpoint_path, map_location='cuda:0')
+
+        # Create a new dictionary to store the filtered layers that we want to load
+        filtered_state_dict = {}
+        # Specify the layers we want to load (for example, we only want to load layer1 and layer2)
+        # Iterate over the state dictionary and filter out the layers we want
+        if layers_to_load:
+            for layer_name in state_dict:
+                if layer_name in layers_to_load:
+                    # print("load", layer_name)
+                    filtered_state_dict[layer_name] = state_dict[layer_name]
+        else:  # load all
+            for layer_name in state_dict:
+                # print("load", layer_name)
+                filtered_state_dict[layer_name] = state_dict[layer_name]
+
+        print("Param names trying to load")
+        for name in filtered_state_dict:
+            print(name)
+
+        # Load the filtered state dictionary into the model
+        self.load_state_dict(filtered_state_dict, strict=False)
+
+        # Freeze the layers with preloaded weights
+        # if layers_to_load:
+        #     for name, param in self.named_parameters():
+        #         if name in layers_to_load:
+        #             param.requires_grad = False
+        # else:
+        #     for name, param in self.named_parameters():
+        #         param.requires_grad = False
+
+        # 'strict=False' allows partial loading, meaning if some layers in the model are missing
+        # from the filtered_state_dict, it will not raise an error.
+
+    def load_weights_main_virtual(self):
+        # make sure main and virtual systems have the same initial/loaded weights before training
+        for i, subnet in enumerate(self.subnets_v):
+            if subnet.type == 'pe':
+                subnet.edge_layer.weight.data = self.subnets[i].edge_layer.weight.data.clone()
+                subnet.sudo_node_layer.weight.data = self.subnets[i].sudo_node_layer.weight.data.clone()
+            elif subnet.type == 'pn' or subnet.type == 'dn':
+                subnet.node_layer.weight.data = self.subnets[i].node_layer.weight.data.clone()
+                subnet.sudo_node_layer.weight.data = self.subnets[i].sudo_node_layer.weight.data.clone()
+
+    def exchange_weights(self):
+        for i, subnet in enumerate(self.subnets):
+            if subnet.type == 'pe':
+                subnet.sudo_node_layer.weight.data = self.subnets_v[i].sudo_node_layer.weight.data.clone()
+            elif subnet.type == 'pn' or subnet.type == 'dn':
+                subnet.sudo_node_layer.weight.data = self.subnets_v[i].sudo_node_layer.weight.data.clone()
+
+        for i, subnet in enumerate(self.subnets_v):
+            if subnet.type == 'pe':
+                subnet.edge_layer.weight.data = self.subnets[i].edge_layer.weight.data.clone()
+            elif subnet.type == 'pn' or subnet.type == 'dn':
+                subnet.node_layer.weight.data = self.subnets[i].node_layer.weight.data.clone()
+
+    def exchange_lr(self):
+        for i, subnet in enumerate(self.subnets_v):
+            if subnet.type == 'pe':
+                subnet.lr = self.subnets[i].lr
+            elif subnet.type == 'pn' or subnet.type == 'dn':
+                subnet.lr = self.subnets[i].lr
+
+    def forward(self, data):
+        node_feature = data.x
+        edge_feature = data.edge_attr
+        # particle_effect = torch.zeros((node_feature.size(0), 100)).to(DEVICE)
+        for subnet in self.subnets:
+            if subnet.type == 'ee':
+                node_out, edge_feature = subnet.forward(node_feature, node_feature, data.edge_index, edge_feature)
+            elif subnet.type == 'en':
+                node_out, node_feature = subnet.forward(node_feature)
+            elif subnet.type == 'pe':
+                node_out, edge_feature, input_to_node = subnet.forward(node_feature, node_feature, data.edge_index, edge_feature)
+            elif subnet.type == 'pn':
+                node_out, node_feature = subnet.forward(input_to_node)
+            elif subnet.type == 'dn':
+                node_out, _ = subnet.forward(node_feature)
+        return node_out
+
+    def forward_to_subnet(self, data, last_net):
+        node_feature = data.x
+        edge_feature = data.edge_attr
+        # particle_effect = torch.zeros((node_feature.size(0), 100)).to(DEVICE)
+        for i, subnet in enumerate(self.subnets):
+            if i<=last_net:
+                if subnet.type == 'ee':
+                    node_out, edge_feature = subnet.forward(node_feature, node_feature, data.edge_index, edge_feature)
+                elif subnet.type == 'en':
+                    node_out, node_feature = subnet.forward(node_feature)
+                elif subnet.type == 'pe':
+                    node_out, edge_feature, input_to_node = subnet.forward(node_feature, node_feature, data.edge_index,
+                                                                           edge_feature)
+                elif subnet.type == 'pn':
+                    node_out, node_feature = subnet.forward(input_to_node)
+                elif subnet.type == 'dn':
+                    node_out, _ = subnet.forward(node_feature)
+            else:
+                continue
+        return node_out
+
+    def train_sub_node(self, layer, x, y, lr=0.001):
+        node_out = self.layers[layer].train(x, y, lr=lr)
+        return node_out
+
+    def train_all_subnets(self, data, check_conv=True):
+        i_range = self.args.train_nets
+        node_feature = data.x
+        edge_feature = data.edge_attr
+        particle_effect = torch.zeros((node_feature.size(0), 100)).to(DEVICE)
+        for i, subnet in enumerate(self.subnets):
+            if subnet.type == 'ee':
+                if i in i_range:
+                    node_out, edge_feature = subnet.train(node_feature, node_feature, data.edge_index, edge_feature, data.y, check_conv)
+                else:
+                    _, edge_feature = subnet(node_feature, node_feature, data.edge_index, edge_feature)
+            elif subnet.type == 'en':
+                if i in i_range:
+                    node_out, node_feature = subnet.train(node_feature, data.y, check_conv)
+                else:
+                    _, node_feature = subnet(node_feature)
+            elif subnet.type == 'pe':
+                if i in i_range:
+                    node_out, edge_feature, input_to_node = subnet.train(node_feature, node_feature, data.edge_index,
+                                                                           edge_feature, data.y, check_conv)
+                else:
+                    _, edge_feature, input_to_node = subnet(node_feature, node_feature, data.edge_index,
+                                                                           edge_feature)
+            elif subnet.type == 'pn':
+                if i in i_range:
+                    node_out, node_feature = subnet.train(input_to_node, data.y, check_conv)
+                else:
+                    _, node_feature = subnet(input_to_node)
+            elif subnet.type == 'dn':
+                if i in i_range:
+                    node_out, _ = subnet.train(node_feature, data.y, check_conv)
+                else:
+                    _,_ = subnet(node_feature)
+
+        loss = loss_fn(node_out, data.y)
+        return loss
+
+
+    def train_all_subnets_v(self, data, check_conv=True):
+        i_range = self.args.train_nets
+        node_feature = data.x
+        edge_feature = data.edge_attr
+        particle_effect = torch.zeros((node_feature.size(0), 100)).to(DEVICE)
+        for i, subnet in enumerate(self.subnets_v):
+            if subnet.type == 'ee':
+                if i in i_range:
+                    node_out, edge_feature = subnet.train(node_feature, node_feature, data.edge_index, edge_feature, data.y, check_conv)
+                else:
+                    _, edge_feature = subnet(node_feature, node_feature, data.edge_index, edge_feature)
+            elif subnet.type == 'en':
+                if i in i_range:
+                    node_out, node_feature = subnet.train(node_feature, data.y, check_conv)
+                else:
+                    _, node_feature = subnet(node_feature)
+            elif subnet.type == 'pe':
+                if i in i_range:
+                    node_out, edge_feature, input_to_node = subnet.train(node_feature, node_feature, data.edge_index,
+                                                                           edge_feature, data.y, check_conv)
+                else:
+                    _, edge_feature, input_to_node = subnet(node_feature, node_feature, data.edge_index,
+                                                                           edge_feature)
+            elif subnet.type == 'pn':
+                if i in i_range:
+                    node_out, node_feature = subnet.train(input_to_node, data.y, check_conv)
+                else:
+                    _, node_feature = subnet(input_to_node)
+            elif subnet.type == 'dn':
+                if i in i_range:
+                    node_out, _ = subnet.train(node_feature, data.y, check_conv)
+                else:
+                    _,_ = subnet(node_feature)
+
+        loss = loss_fn(node_out, data.y)
+        return loss
+
